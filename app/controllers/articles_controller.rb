51b8a7c5f1034e0bf21d2ba50c81977d3f1d67c1
Henryk <jatranscedentalne@gmail.com>
2018-08-19 11:25:52 +0200

    add validation for article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,20 @@
 class ArticlesController < ApplicationController
   def index
-
+    @articles = Article.all.order(created_at: :desc)
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     article_params = params.require(:article).permit(:title, :text)
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 5}, uniqueness: true
+  validates :text, presence: true
+  validates_uniqueness_of :title, :text
 end
