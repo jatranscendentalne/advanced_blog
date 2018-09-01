2133c837c7a2be57efaaf12d4795c4c036aac1ad
Henryk <jatranscedentalne@gmail.com>
2018-09-01 10:22:43 +0200

    destroy action for comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}, uniqueness: true
   validates :text, presence: true, length: {maximum: 200}
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: %i[create destroy]
   end
 end
