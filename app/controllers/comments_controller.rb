cf536928d04f8cab633a0bd8cd612b84770b4671
Henryk <jatranscedentalne@gmail.com>
2018-09-01 12:35:37 +0200

    improve resource name rendering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,29 @@ class CommentsController <ApplicationController
   before_action :provide_article, only: %i[create destroy]
 
   def create
-    @comment = @article.comments.create(comment_params)
-    redirect_to @article
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to @article
+    else
+      render 'articles/show'
+    end
   end
 
 
   def destroy
-
-  @comment = @article.comments.find(params[:id])
-
-  @comment.destroy!
-
-  redirect_to @article
-end
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy!
+    redirect_to @article
+  end
 
 private
 
-def provide_article
-  @article = Article.find(params[:article_id])
-end
+  def provide_article
+    @article = Article.find(params[:article_id])
+  end
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body).merge(article: @article)
   end
 end
