1c5148c29d84ae50a5d8eddb5759fd0477e22e01
Henryk <jatranscedentalne@gmail.com>
2018-09-01 09:42:24 +0200

    list all comments for article

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  # get 'articles', to: 'articles#index'
-  # get 'articles/new', to: 'articles#new', as: 'new_article'
-  # get 'articles/:id', to: 'articles#show', as: 'article'
-  # get 'articles/ :id/edit', to: 'articles#edit', as: 'edit_article'
-  # post 'articles', to: 'articles#create'
-  # patch 'articles/:id', to: 'articles#update'
-  # delete 'articles/:id', to: 'articles#destroy'
-  resources :articles
-  resources :comments
+  resources :articles do
+    resources :comments, only: [:create]
+  end
 end
